11a9117b75dd59d2ac473857f81fad00d0872076
tanishraj <tanish.quitegrowth@gmail.com>
2021-06-09 11:29:10 +0530

    updated

--- code/even-or-odd-without-condition.js
+++ code/even-or-odd-without-condition.js
@@ -6,3 +6,12 @@ function evenOrOdd(num) {
 console.log(evenOrOdd(10));
 console.log(evenOrOdd(45));
 console.log(evenOrOdd(66));
+
+// Another Approach
+function evenOrOdd2(num) {
+  return (num & 1 && "odd") || "even";
+}
+
+console.log(evenOrOdd2(1));
+console.log(evenOrOdd2(45));
+console.log(evenOrOdd2(66));
